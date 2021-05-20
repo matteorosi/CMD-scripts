987f4b6c4da71626573cc50510365b55c35a7ee7
matteorosi <53863633+matteorosi@users.noreply.github.com>
2021-05-20 17:00:13 +0200

    Clear Spooler

---
+++ Spooler.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Stopping print spooler.
+echo.
+net stop spooler
+echo deleting temp files.
+echo.
+ping localhost -n 4 > nul
+del %windir%\system32\spool\printers\*.* /q
+echo Starting print spooler.
+echo.
+net start spooler
+ping localhost -n 4 > nul
+echo Done!
