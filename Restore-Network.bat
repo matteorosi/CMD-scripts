9aed2246e821e512fbbc587bf3036ae5e8d29415
matteorosi <53863633+matteorosi@users.noreply.github.com>
2021-05-20 14:28:49 +0200

    Delete Restore-Network.bat

--- Restore-Network.bat
+++
@@ -1,9 +0,0 @@
-netsh int ip reset c:\resetlog.txt
-netsh winsock reset
-netsh advfirewall reset
-netsh winhttp reset proxy
-arp –d *
-ipconfig /release
-ipconfig /renew
-ipconfig /flushdns
-ipconfig /registerdns
